6e8a6df7653bfcc87f23fce69101625b03433fd1
Gary Spagnoli <gary.spagnoli@gmail.com>
2014-10-04 17:33:06 -0400

    config modification for heroku

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.serve_static_assets = true
 
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,6 +10,8 @@ Bundler.require(*Rails.groups)
 
 module Caborsubway
   class Application < Rails::Application
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
